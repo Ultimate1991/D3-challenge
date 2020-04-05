e1a74caa38aed9ced87b290d7f170f59f0bc986e
Ultimate1991 <57298395+Ultimate1991@users.noreply.github.com>
2020-04-05 13:25:23 -0700

    code commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,41 @@
-// settings
-var svgWidth = 800;
+var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
   top: 20,
   right: 40,
-  bottom: 80,
+  bottom: 60,
   left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// basic svg setup
+// Create an SVG wrapper, append an SVG group that will hold the chart.
 var svg = d3
   .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-// chart group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-
+// Import Data
 d3.csv("/assets/data/data.csv").then(function(myData) {
     
-    // convert data to numbers
-    myData.forEach(function(xdata) {
-        xdata.poverty = +xdata.poverty;
-        xdata.healthcare = +xdata.healthcare;
-        //console.log(xdata.state,xdata.abbr,xdata.poverty,xdata.healthcare);
+    // Parse Data/Cast as numbers
+    myData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
     });
 
-    // set x scale function
+    // Create scale functions
     var xLinearScale = d3.scaleLinear()
         .domain([d3.min(myData, d=>d.poverty)*0.9, 
             d3.max(myData, d => d.poverty)*1.1])
         .range([0, width]);
 
-    // set y scale function
     var yLinearScale = d3.scaleLinear()
         .domain([0, d3.max(myData, d => d.healthcare)*1.1])
         .range([height, 0]);
@@ -48,27 +44,26 @@ d3.csv("/assets/data/data.csv").then(function(myData) {
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    // append x axis
+    // Append Axes to the chart
     chartGroup.append("g")
         .attr("transform", `translate(0, ${height})`)
         .style("font-size", "18px")
         .call(bottomAxis);
 
-    // append y axis
     chartGroup.append("g")
         .style("font-size", "18px")
         .call(leftAxis);
   
-    // circles
+    // Create circles
     chartGroup.selectAll("circle")
         .data(myData)
         .enter()
         .append("circle")
         .attr("cx", d => xLinearScale(d.poverty))
         .attr("cy", d => yLinearScale(d.healthcare))
-        .attr("r", 12)
+        .attr("r", "15")
         .attr("fill", "blue")
-        .attr("opacity", ".3");
+        .attr("opacity", ".5");
 
     // text with in circles
     chartGroup.selectAll("text.text-circles")
@@ -79,7 +74,7 @@ d3.csv("/assets/data/data.csv").then(function(myData) {
         .text(d => d.abbr)
         .attr("x", d => xLinearScale(d.poverty))
         .attr("y", d => yLinearScale(d.healthcare))
-        .attr("dy",5)
+        .attr("dy","5")
         .attr("text-anchor","middle")
         .attr("font-size","12px");
 
